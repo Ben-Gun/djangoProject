ed34d97cbafbbd30c759dac610d5e3fce6440143
Ben_Gun <dmitriy.titenok@gmail.com>
2023-03-20 18:57:24 +0300

    1

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+def maps(request):
+    return render(request, 'main/maps.html')
